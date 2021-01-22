54ac10a342804d5809873fa60dbda31479cfcddb
Cameron King <cking@ckxng.com>
2021-01-21 23:50:41 -0600

    adds character form submission

---
+++ cyphergen/static/js/generate.js
@@ -0,0 +1,62 @@
+$('#sheet-form').submit(function( event ) {
+    $('#form-warning').hide();
+    $('#form-success').hide();
+    var sheet = {
+        "name": $('#name').val(),
+        "type": $('#type').val(),
+        "tier": $.isNumeric($('#tier').val()) ? parseInt($('#tier').val()) : 0,
+        "might": $.isNumeric($('#might').val()) ? parseInt($('#might').val()) : 0,
+        "speed": $.isNumeric($('#speed').val()) ? parseInt($('#speed').val()) : 0,
+        "intellect": $.isNumeric($('#intellect').val()) ? parseInt($('#intellect').val()) : 0,
+        "effort": $.isNumeric($('#effort').val()) ? parseInt($('#effort').val()) : 0,
+        "edge": {
+            "intellect": $.isNumeric($('#edgeintellect').val()) ? parseInt($('#edgeintellect').val()) : 0,
+            "might": $.isNumeric($('#edgemight').val()) ? parseInt($('#edgemight').val()) : 0,
+            "speed": $.isNumeric($('#edgespeed').val()) ? parseInt($('#edgespeed').val()) : 0
+        },
+        "abilities": [],
+        "equipment": $('#equipment').val().split("\n"),
+        "skills": {}
+    };
+    console.log(sheet);
+    $('input[name="abilities[]"]:checked').each(function(){
+        sheet.abilities.push($(this).val());
+    });
+
+    var skill_list = $('#skill-list').val().split("\n");
+    for(var i = 0; i < skill_list.length; i++) {
+        var val = $(`input:radio[name=skill-${skill_list[i]}]:checked`).val()
+        if(parseInt(val) != 0) {
+            sheet.skills[skill_list[i]] = val
+        }
+    }
+    console.log(sheet);
+
+    try {
+        $.ajax({
+            url: '/api/v1/characters',
+            type: 'POST',
+            data: JSON.stringify(sheet),
+            dataType: 'json',
+            contentType: "application/json"
+        }).always(function(data, textStatus, jqXHR) {
+            console.log(JSON.stringify(data));
+            if(jqXHR.status == 201) {
+                $('#form-success').html(
+                    'Your character can be viewed at this URL:<br>'+
+                    `<a href="${data.web}">${data.web}</a><br>`+
+                    'Your submitted character parameters can be viewed '+
+                    `<a href="${data.api}">here</a>.`);
+                $('#form-success').show();
+            } else {
+                $('#form-warning').html("The server failed to process the request.  Check to make sure your input matches the template." );
+                $('#form-warning').show();
+            }
+        })
+    } catch(error) {
+        $('#form-warning').html("Failed to submit the form to the server."+error);
+        $('#form-warning').show();
+    }
+    
+    event.preventDefault();
+  });
